ae1b574c0e50c046bb7dc94b4656bf4c4852e927
Shah Rukh Khan <shah@crowdbotics.com>
2020-08-25 08:51:34 +0000

    Update models

--- users/migrations/0005_remove_user_new_title.py
+++ users/migrations/0005_remove_user_new_title.py
@@ -6,12 +6,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0004_user_df'),
+        ("users", "0004_user_df"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='user',
-            name='new_title',
-        ),
+        migrations.RemoveField(model_name="user", name="new_title",),
     ]
--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
-    df = models.BigIntegerField(null=True, blank=True,)
+    dftest = models.BigIntegerField(null=True, blank=True,)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
